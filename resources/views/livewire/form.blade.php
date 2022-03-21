2ee26c7470f912af7fbe4ba613430541d95f2c33
Adrian Moreno <adrianemoreno.25@gmail.com>
2022-03-20 20:23:07 -0400

    Working Stable

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,97 +1,89 @@
-<div>
-	<div id="form" class="modal" tabindex="-1" role="dialog" wire:ignore.self>
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
+<div class="col-3 p-4">
 
-				<div class="modal-header">
-					<h5 class="modal-title">{{ $type == 0 ? "Agregar" : "Editar" }} Articulo</h5>
-				</div>{{-- Modal Header --}}
+	<div class="card">
+		<div class="card-header">
+			<h3>{{ $type == 0 ? "Agregar" : "Editar" }} Articulo</h3>
+		</div>
 
-				<div class="modal-body">
-					<div>
-						{{-- Article Name --}}
-						<div class="mb-3">      
-							<label for="name" class="form-label">Nombre:</label>        
-							<input type="text" class="form-control" id="name" wire:model.defer="name">  
-							@error('name')
-							<span class="error">Debe Ingresar el nombre del articulo</span>
-							@enderror
-							<div id="nameHelp" class="form-text">Ingrese el nombre del articulo</div>
-						</div>
-
-						{{-- Article Description --}}
-						<div class="mb-3">      
-							<label for="description" class="form-label">Descripcion:</label>        
-							<input type="text" class="form-control" id="description" wire:model.defer="description">    
-							@error('description')
-							<span class="error">Debe Ingresar la descripcion del articulo</span>
-							@enderror
-							<div id="descriptionHelp" class="form-text">
-								Ingrese una descripcion del articulo
-							</div>
-						</div>
-
-						{{-- Article Categorie --}}
-						<div class="mb-3">      
-							<label for="categorie" class="form-label">Categoria:</label>        
-							<input type="text" class="form-control" id="categorie"wire:model.defer="categorie">
-							@error('categorie')
-							<span class="error">Debe Ingresar la categoria del articulo</span>
-							@enderror
-							<div id="categorieHelp" class="form-text">
-								Ingrese una categoria del articulo
-							</div>
-						</div>
-
-						<div class="row">
-
-							<div class="col">
-								{{-- Article Amount --}}
-								<div class="mb-3">      
-									<label for="amount" class="form-label">Cantidad:</label>        
-									<input type="number" class="form-control" id="amount" wire:model.defer="amount">    
-									@error('amount')
-									<span class="error">Debe Ingresar la cantidad del articulo</span>
-									@enderror
-									<div id="amountHelp" class="form-text">
-										Ingrese la cantidad del articulo
-									</div>
-								</div>
-							</div>
-
-							<div class="col">
-								{{-- Article Price --}}
-								<div class="mb-3">      
-									<label for="price" class="form-label">Precio:</label>       
-									<input type="number" class="form-control" id="price" step="0.01" wire:model.defer="price">  
-									@error('price')
-									<span class="is-invalid">Debe Ingresar el precio del articulo</span>
-									@enderror
-									<div id="priceHelp" class="form-text">
-										Ingrese el precio del articulo
-									</div>
-								</div>
-							</div>
-
-						</div>{{-- Row --}}
-
-					</div>{{-- MAIN --}}
+		<div class="card-body">
 
 
-
-
+			<div>
+				{{-- Article Name --}}
+				<div class="mb-3">      
+					<label for="name" class="form-label">Nombre:</label>        
+					<input type="text" class="form-control" id="name" wire:model.defer="name">  
+					@error('name')
+					<span class="error">Debe Ingresar el nombre del articulo</span>
+					@enderror
+					<div id="nameHelp" class="form-text">Ingrese el nombre del articulo</div>
 				</div>
 
-				<div class="modal-footer">
-					<button wire:click.prevent="addArticle" class="btn btn-primary">
-						{{ $type == 0 ? "Agregar" : "Actualizar" }}
-					</button>
-					<button wire:click.prevent="closeForm" type="button" class="btn btn-secondary">
-						Cancelar
-					</button>
-				</div>{{-- Modal Footer --}}
+				{{-- Article Description --}}
+				<div class="mb-3">      
+					<label for="description" class="form-label">Descripcion:</label>        
+					<input type="text" class="form-control" id="description" wire:model.defer="description">    
+					@error('description')
+					<span class="error">Debe Ingresar la descripcion del articulo</span>
+					@enderror
+					<div id="descriptionHelp" class="form-text">
+						Ingrese una descripcion del articulo
+					</div>
+				</div>
 
-			</div>{{-- Modal Content --}}
+				{{-- Article Categorie --}}
+				<div class="mb-3">      
+					<label for="categorie" class="form-label">Categoria:</label>        
+					<input type="text" class="form-control" id="categorie"wire:model.defer="categorie">
+					@error('categorie')
+					<span class="error">Debe Ingresar la categoria del articulo</span>
+					@enderror
+					<div id="categorieHelp" class="form-text">
+						Ingrese una categoria del articulo
+					</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col">
+						{{-- Article Amount --}}
+						<div class="mb-3">      
+							<label for="amount" class="form-label">Cantidad:</label>        
+							<input type="number" class="form-control" id="amount" wire:model.defer="amount">    
+							@error('amount')
+							<span class="error">Debe Ingresar la cantidad del articulo</span>
+							@enderror
+							<div id="amountHelp" class="form-text">
+								Ingrese la cantidad del articulo
+							</div>
+						</div>
+					</div>
+
+					<div class="col">
+						{{-- Article Price --}}
+						<div class="mb-3">      
+							<label for="price" class="form-label">Precio:</label>       
+							<input type="number" class="form-control" id="price" step="0.01" wire:model.defer="price">  
+							@error('price')
+							<span class="is-invalid">Debe Ingresar el precio del articulo</span>
+							@enderror
+							<div id="priceHelp" class="form-text">
+								Ingrese el precio del articulo
+							</div>
+						</div>
+					</div>
+
+				</div>{{-- Row --}}
+
+			</div>
+		</div>{{-- Card Body --}}
+
+		<div class="card-footer text-center">
+			<button wire:click.prevent="addArticle" class="btn btn-primary">
+				{{ $type == 0 ? "Agregar" : "Actualizar" }}
+			</button>
 		</div>
-	</div>{{-- Modal Base --}}
-</div>
+
+	</div>{{-- Card --}}
+
+</div>{{-- MAIN --}}
--- resources/views/livewire/table.blade.php
+++ resources/views/livewire/table.blade.php
@@ -1,63 +1,63 @@
-<div class="col p-4">
-	<div class="card text-center">
-		<div class="card-header">
-			<h3>Articulos</h3>
-		</div>
-		<div class="card-body">
-			<table class="table table-striped">
-				<thead class="text-center">
-					<tr>
-						<th class="text-center align-middle" scope="col">#</th>
-						<th class="text-center align-middle" scope="col">Nombre</th>
-						<th class="text-center align-middle" scope="col">Descripcion</th>
-						<th class="text-center align-middle" scope="col">Categoria</th>
-						<th class="text-center align-middle" scope="col">Cantidad</th>
-						<th class="text-center align-middle" scope="col">Precio</th>
-						<th class="text-center align-middle" scope="col"></th>
-					</tr>
-				</thead>
+<div class="container">
+	<div class="row">
 
-				<tbody>
+		@include('livewire.form')
 
-					@foreach($articles as $a)
-					<tr>
-						<td class="text-center align-middle">{{ $a->id }}</td>
-						<td class="text-center align-middle">{{ $a->name }}</td>
-						<td class="text-center align-middle">{{ $a->description }}</td>
-						<td class="text-center align-middle">{{ $a->categorie }}</td>
-						<td class="text-center align-middle">{{ $a->amount }}</td>
-						<td class="text-center align-middle">{{ $a->price }}</td>
-						<td>
-							<div class="btn-group" role="group">
-								<button wire:click="editArticle({{ $a->id }})" type="button" class="btn btn-info">Editar</button>
-								<button wire:click="confirmDeleteArticle({{ $a->id }})" type="button" class="btn btn-danger">Borrar</button>
-							</div>
-						</td>
-					</tr>
-					@endforeach
+		<div class="col-9 p-4">
+			<div class="card text-center">
+				<div class="card-header">
+					<h3>Articulos</h3>
+				</div>
+				<div class="card-body">
+					<table class="table table-striped">
+						<thead class="text-center">
+							<tr>
+								<th class="text-center align-middle" scope="col">#</th>
+								<th class="text-center align-middle" scope="col">Nombre</th>
+								<th class="text-center align-middle" scope="col">Descripcion</th>
+								<th class="text-center align-middle" scope="col">Categoria</th>
+								<th class="text-center align-middle" scope="col">Cantidad</th>
+								<th class="text-center align-middle" scope="col">Precio</th>
+								<th class="text-center align-middle" scope="col"></th>
+							</tr>
+						</thead>
 
-					
-				</tbody>{{-- Table BOdy --}}
+						<tbody>
 
-			</table>{{-- Table --}}
-		</div>{{-- Card Body --}}
+							@foreach($articles as $a)
+							<tr>
+								<td class="text-center align-middle">{{ $a->id }}</td>
+								<td class="text-center align-middle">{{ $a->name }}</td>
+								<td class="text-center align-middle">{{ $a->description }}</td>
+								<td class="text-center align-middle">{{ $a->categorie }}</td>
+								<td class="text-center align-middle">{{ $a->amount }}</td>
+								<td class="text-center align-middle">{{ $a->price }}</td>
+								<td>
+									<div class="btn-group" role="group">
+										<button wire:click="editArticle({{ $a->id }})" type="button" class="btn btn-info">Editar</button>
+										<button wire:click="confirmDeleteArticle({{ $a->id }})" type="button" class="btn btn-danger">Borrar</button>
+									</div>
+								</td>
+							</tr>
+							@endforeach
 
-		<div class="card-footer text-muted">
 
-			<div class="container">
-				{{ $articles->links('layouts.pagination') }}
-			</div>
+						</tbody>{{-- Table Body --}}
 
-		</div>{{-- Card Footer --}}
-	</div>{{-- Card --}}
+					</table>{{-- Table --}}
+				</div>{{-- Card Body --}}
 
-	<div class="container text-center mt-2">
-		<button wire:click="openForm" class="btn btn-primary">Agregar</button>
-	</div>
+				<div class="card-footer text-muted">
+					<div class="container">
+						{{ $articles->links('layouts.pagination') }}
+					</div>
+				</div>{{-- Card Footer --}}
 
-	@include('livewire.form')
+			</div>{{-- Card --}}
 
-</div> {{-- col-9 --}}
+		</div> {{-- col-9 --}}
+	</div>{{-- Row --}}
+</div>{{-- Container --}}
 
 <script>
 	window.addEventListener('openForm', e => {
@@ -75,7 +75,7 @@
 			'deleteArticle', 
 			confirm("Esta seguro de borrar " + event.detail.name + '?'),
 			event.detail.id
-		)
+			)
 	});
 </script>
 
